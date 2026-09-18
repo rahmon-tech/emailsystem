35b8ad86821bfbed068be69b56dce96350382ec4
Rahmony <85800671+rahmon-tech@users.noreply.github.com>
2026-09-18 09:24:08 +0100

    fix(ui): use typed select menu scroll props

--- apps/web/components/blast.tsx
+++ apps/web/components/blast.tsx
@@ -457,7 +457,6 @@ export function Blast() {
                             },
                           },
                         },
-                        MenuListProps: { sx: { py: 0.5 } },
                       },
                     },
                   }}
